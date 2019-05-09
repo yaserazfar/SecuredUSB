5b67f00af456488f48d565c83c73d4905cfe486d
Yaser Azfar <yaserazfar@gmail.com>
2019-05-10 07:53:33 +1200

    initial commit

---
+++ Serial Port Application/Serial Port Application/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Serial_Port_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextBoxCommunication = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonAccept = new System.Windows.Forms.Button();
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(357, 51);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(165, 49);
+            this.buttonConnect.TabIndex = 1;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Communication:";
+            // 
+            // TextBoxCommunication
+            // 
+            this.TextBoxCommunication.Location = new System.Drawing.Point(12, 51);
+            this.TextBoxCommunication.Name = "TextBoxCommunication";
+            this.TextBoxCommunication.Size = new System.Drawing.Size(336, 269);
+            this.TextBoxCommunication.TabIndex = 3;
+            this.TextBoxCommunication.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(354, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Connection Status:";
+            // 
+            // buttonAccept
+            // 
+            this.buttonAccept.Enabled = false;
+            this.buttonAccept.Location = new System.Drawing.Point(354, 215);
+            this.buttonAccept.Name = "buttonAccept";
+            this.buttonAccept.Size = new System.Drawing.Size(165, 49);
+            this.buttonAccept.TabIndex = 6;
+            this.buttonAccept.Text = "Accept Connection";
+            this.buttonAccept.UseVisualStyleBackColor = true;
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.Location = new System.Drawing.Point(354, 123);
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.Size = new System.Drawing.Size(200, 22);
+            this.textBoxStatus.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 338);
+            this.Controls.Add(this.textBoxStatus);
+            this.Controls.Add(this.buttonAccept);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TextBoxCommunication);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox TextBoxCommunication;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonAccept;
+        private System.Windows.Forms.TextBox textBoxStatus;
+    }
+}
+
---
+++ Serial Port Application/Serial Port Application/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Serial_Port_Application
+{
+    public partial class Form1 : Form
+    {
+        static SerialPort port;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);              //REPLACE COM7 WITH YOUR BLUETOOTH PORT (DeviceManager->Ports->StandardBluetooth...)
+            textBoxStatus.Text = "Please unlock your phone...";
+
+            port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);     //When the serial port receives data (from the phone), it will call the function "port_DataReceived"
+            port.Open();
+
+            while (true)
+            {
+                port.WriteLine(TextBoxCommunication.Text);     //Write whatever we enter into console to the serial port (send to phone)
+            }
+        }
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Console.WriteLine(port.ReadExisting());         //Write whatever the phone sent
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+            textBoxStatus.Text = "Unlocked...";
+        }
+    }
+}
+
