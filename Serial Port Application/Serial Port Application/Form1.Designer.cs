48de7b6eb570e0393b1ca34952584e1cff1e6933
Yaser Azfar <yaserazfar@gmail.com>
2019-05-22 09:57:05 +1200

    Add generic USB names to listbox when a new usb is added

--- Serial Port Application/Serial Port Application/Form1.Designer.cs
+++ Serial Port Application/Serial Port Application/Form1.Designer.cs
@@ -28,92 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.buttonConnect = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.TextBoxCommunication = new System.Windows.Forms.RichTextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.buttonAccept = new System.Windows.Forms.Button();
-            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.buttonDecrypt = new System.Windows.Forms.Button();
+            this.listBoxUsb = new System.Windows.Forms.ListBox();
+            this.labelStatus = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // buttonConnect
-            // 
-            this.buttonConnect.Location = new System.Drawing.Point(357, 51);
-            this.buttonConnect.Name = "buttonConnect";
-            this.buttonConnect.Size = new System.Drawing.Size(165, 49);
-            this.buttonConnect.TabIndex = 1;
-            this.buttonConnect.Text = "Connect";
-            this.buttonConnect.UseVisualStyleBackColor = true;
-            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Location = new System.Drawing.Point(39, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 17);
+            this.label1.Size = new System.Drawing.Size(95, 17);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Communication:";
+            this.label1.Text = "Select a USB:";
             // 
-            // TextBoxCommunication
+            // buttonEncrypt
             // 
-            this.TextBoxCommunication.Location = new System.Drawing.Point(12, 51);
-            this.TextBoxCommunication.Name = "TextBoxCommunication";
-            this.TextBoxCommunication.Size = new System.Drawing.Size(336, 269);
-            this.TextBoxCommunication.TabIndex = 3;
-            this.TextBoxCommunication.Text = "";
+            this.buttonEncrypt.Location = new System.Drawing.Point(42, 178);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(146, 44);
+            this.buttonEncrypt.TabIndex = 8;
+            this.buttonEncrypt.Text = "Encrypt Files";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
             // 
-            // label2
+            // buttonDecrypt
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(354, 103);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(127, 17);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Connection Status:";
+            this.buttonDecrypt.Location = new System.Drawing.Point(248, 178);
+            this.buttonDecrypt.Name = "buttonDecrypt";
+            this.buttonDecrypt.Size = new System.Drawing.Size(146, 44);
+            this.buttonDecrypt.TabIndex = 9;
+            this.buttonDecrypt.Text = "Decrypt Files";
+            this.buttonDecrypt.UseVisualStyleBackColor = true;
+            this.buttonDecrypt.Click += new System.EventHandler(this.buttonDecrypt_Click);
             // 
-            // buttonAccept
+            // listBoxUsb
             // 
-            this.buttonAccept.Enabled = false;
-            this.buttonAccept.Location = new System.Drawing.Point(354, 215);
-            this.buttonAccept.Name = "buttonAccept";
-            this.buttonAccept.Size = new System.Drawing.Size(165, 49);
-            this.buttonAccept.TabIndex = 6;
-            this.buttonAccept.Text = "Accept Connection";
-            this.buttonAccept.UseVisualStyleBackColor = true;
+            this.listBoxUsb.FormattingEnabled = true;
+            this.listBoxUsb.ItemHeight = 16;
+            this.listBoxUsb.Location = new System.Drawing.Point(42, 54);
+            this.listBoxUsb.Name = "listBoxUsb";
+            this.listBoxUsb.Size = new System.Drawing.Size(352, 100);
+            this.listBoxUsb.TabIndex = 10;
             // 
-            // textBoxStatus
+            // labelStatus
             // 
-            this.textBoxStatus.Location = new System.Drawing.Point(354, 123);
-            this.textBoxStatus.Name = "textBoxStatus";
-            this.textBoxStatus.Size = new System.Drawing.Size(200, 22);
-            this.textBoxStatus.TabIndex = 7;
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(39, 225);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(0, 17);
+            this.labelStatus.TabIndex = 11;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(572, 338);
-            this.Controls.Add(this.textBoxStatus);
-            this.Controls.Add(this.buttonAccept);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.TextBoxCommunication);
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(447, 262);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.listBoxUsb);
+            this.Controls.Add(this.buttonDecrypt);
+            this.Controls.Add(this.buttonEncrypt);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.buttonConnect);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "DIY USB";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button buttonConnect;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RichTextBox TextBoxCommunication;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button buttonAccept;
-        private System.Windows.Forms.TextBox textBoxStatus;
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.Button buttonDecrypt;
+        private System.Windows.Forms.ListBox listBoxUsb;
+        private System.Windows.Forms.Label labelStatus;
     }
 }
 
--- Serial Port Application/Serial Port Application/Form1.cs
+++ Serial Port Application/Serial Port Application/Form1.cs
@@ -7,31 +7,59 @@ namespace Serial_Port_Application
     public partial class Form1 : Form
     {
         static SerialPort port;
+        public static int UniqueUsbKey = 0; //this is a key that will be given to every usb and is different for each usb *would potentially be in the cloud*
+        //public static List<int> UsbKeyList = new List<int>();
 
         public Form1()
         {
             InitializeComponent();
-        }
-        
-        private void buttonConnect_Click(object sender, EventArgs e)
-        {
-            port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);              //REPLACE COM7 WITH YOUR BLUETOOTH PORT (DeviceManager->Ports->StandardBluetooth...)
-            textBoxStatus.Text = "Please unlock your phone...";
+            port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);              //REPLACE COM3 WITH YOUR BLUETOOTH PORT (DeviceManager->Ports->StandardBluetooth...)
 
             port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);     //When the serial port receives data (from the phone), it will call the function "port_DataReceived"
             port.Open();
 
-            while (true)
+        }
+        
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string recievedString = (port.ReadExisting());
+            Console.WriteLine(recievedString);         //Write whatever the phone sent
+
+            if (recievedString == "1") //this is the integer used to represent when user is trying to sync usb
             {
-                port.WriteLine(TextBoxCommunication.Text);     //Write whatever we enter into console to the serial port (send to phone)
+                UniqueUsbKey++;
+                //port.WriteLine("USB Connected");
+                port.WriteLine(UniqueUsbKey.ToString());
+
+                //UsbKeyList.Add(UniqueUsbKey);
+                
+                TextBox.CheckForIllegalCrossThreadCalls = false;
+                listBoxUsb.Items.Add("USB #" + UniqueUsbKey);
+            }
+
+            if (recievedString == "2")  //once the decrypt button is clicked, it will notify user on mobile and if yes then 2
+            {
+                //TextBox.CheckForIllegalCrossThreadCalls = false;
+                //labelStatus.Text = "Decrypting files...";
+
+                //===========------------TO DO DANIEL-------================
+                //decrypt usb information onto pc 
             }
         }
 
-        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        private void buttonEncrypt_Click(object sender, EventArgs e)
         {
-            Console.WriteLine(port.ReadExisting());         //Write whatever the phone sent
-            TextBox.CheckForIllegalCrossThreadCalls = false;
-            textBoxStatus.Text = "Unlocked...";
+            //TextBox.CheckForIllegalCrossThreadCalls = false;
+            //labelStatus.Text = "Encrypting files...";
+
+            //=============------------TO DO DANIEL-------====================
+            //encrypt the files 
+        }
+
+        private void buttonDecrypt_Click(object sender, EventArgs e)    //sends key to user and promts them to unlock phone 
+        {
+            //send UniqueUsbKey of seleceted USB to port - selected from listbox
+            port.WriteLine(UniqueUsbKey.ToString());
         }
     }
 }
